688a7d7526e298a0b4de3b29e9beb341c0a58de3
Konika13 <konika.agarwal1994@gmail.com>
2017-01-24 14:45:34 +0530

    Add files via upload

---
+++ script6.4.js
@@ -0,0 +1,19 @@
+
+var arr=[{'name':'Konika Agarwal','age':22,'salary':'1 lac','city':'Pune','state':'Maharashtra','pincode':'411001'},
+             {'name':'Gaurav Garg','age':25,'salary':'60k','city':'Lalkuan','state':'Uttarakhand','pincode':'263145'},
+            {'name':'Naveen Goyal','age':27,'salary':'80k','city':'Noida','state':'Uttar Pradesh','pincode':'201301'},
+            {'name':'Vandana Agarwal','age':26,'salary':'1.2 lac','city':'Pune','state':'Maharashtra','pincode':'411014'},
+            {'name':'Vikas Agarwal','age':29,'salary':'60k','city':'Rampur','state':'Uttar Pradesh','pincode':'244921'}];
+
+for(var i=0;i<=4;i++)
+{
+console.log("Employee"+(i+1))
+console.log("Name-"+arr[i].name)
+console.log("Age-"+arr[i].age)
+console.log("Salary-"+arr[i].salary)
+console.log("City-"+arr[i].city)
+console.log("State-"+arr[i].state)
+console.log("Pincode-"+arr[i].pincode)
+}
+
+              
